544e3e0fcf205e57197a1e2805a4a6a471e2ab6a
Lisha <kolhelisha@gmail.com>
2026-06-04 17:27:15 +0530

    Pan number Validation

--- PAN_validation.py
+++ PAN_validation.py
@@ -1,8 +1,32 @@
+def validate_pan(pan):
+    if(len(pan)== 10 and
+       pan[0:5].isalpha() and
+       pan[:5].isupper() and
+       pan[5:9].isdigit() and
+       pan[9].isalpha() and
+       pan[9].isupper()):
+        return True
+    else:
+        return False    
+def pan_category(pan):
+    category_dict = {
+        'A' : "Association of Persons (AOP)",
+        'B' : "Body of Individuals (BOI)",
+        'G' : "Government Agency",
+        'J' : "Artificial Juridical Person",
+        'L' : "Local Authority",
+        'F' : "Firm",
+        'T' : "Trust",
+        'C' : "Company",
+        'P' : "Individual",
+        'H' : "HUF (Hindu Undivided Family)"
+    }
+    category_code = pan[3]
+    return category_dict.get(category_code, "Unknown Category")
 pan = input("Enter your PAN number: ")
-if ((len(pan) == 10) and
-    pan[:5].isalpha() and pan[:5].isupper() and
-    pan[5:9].isdigit() and
-    pan[9].isalpha() and pan[9].isupper()):
-    print("Valid PAN Number")
+if validate_pan(pan):
+    print("Valid PAN number")
+    category = pan_category(pan)
+    print("Category:", category)
 else:
-    print("Invalid PAN Number")
+    print("Invalid PAN number")
