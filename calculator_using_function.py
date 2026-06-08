b25870fdc82aed0408ae16f417428b3f3fb415a0
Lisha <kolhelisha@gmail.com>
2026-06-08 21:21:14 +0530

    Calculator using Function

---
+++ calculator_using_function.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
+
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+
+print("Addition: ", add(num1, num2))
+print("Subtraction: ", subtract(num1, num2))
+print("Multiplication: ", multiply(num1, num2))
+print("Division: ", divide(num1, num2))
